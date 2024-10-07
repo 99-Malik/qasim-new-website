88bab54cd43e7196ada8ae556fa1b9ae1d35a2a5
usama iftikhar <usamaiftikhar718@gmail.com>
2024-10-08 00:46:47 +0500

    site updated

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -15,7 +15,7 @@ export default function MainBanner({ company }) {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 max-w-3xl p-6 text-center bg-gradient-to-r from-black to-transparent rounded-lg shadow-lg mx-6 lg:mx-12">
+      <div className="relative z-10 max-w-4xl p-2 text-center bg-gradient-to-r from-black to-transparent rounded-lg shadow-lg mx-6 lg:mx-12">
         {/* Header Text */}
         <h2 className="text-xl font-extrabold tracking-wide uppercase text-white">
         Appliances
@@ -37,7 +37,7 @@ export default function MainBanner({ company }) {
         </h2>
 
         {/* Subtext */}
-        <p className="mt-4 text-sm text-gray-300">
+        <p className="mt-4 text-md text-center text-gray-300">
           We provide top-notch repair services for {company} appliances with a
           team of certified and experienced technicians.
         </p>
